96e89b595780ac4a1550e27045f34fb3290190ae
Soren Martius <soeren.martius@gmail.com>
2019-12-24 14:16:49 +0100

    use correct repo name for collaborator reference

--- test/github_repository_test.go
+++ test/github_repository_test.go
@@ -18,7 +18,35 @@ func TestTerraformRepository(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
 		// The path to where your Terraform code is located
-		TerraformDir: "../examples/private-repository",
+		TerraformDir: "../examples/public-repository",
+		Upgrade:      true,
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{
+			"github_token":        githubToken,
+			"github_organization": githubOrganization,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+}
+
+func TestTerraformRepositoryWithCollaborators(t *testing.T) {
+	t.Parallel()
+
+	githubOrganization := os.Getenv("GITHUB_ORGANIZATION")
+	githubToken := os.Getenv("GITHUB_TOKEN")
+
+	if githubToken == "" {
+		panic("Please set a github token using the GITHUB_TOKEN environment variable.")
+	}
+
+	terraformOptions := &terraform.Options{
+		// The path to where your Terraform code is located
+		TerraformDir: "../examples/public-repository-with-collaborators",
 		Upgrade:      true,
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
