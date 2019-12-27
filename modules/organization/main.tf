42e36fdb779009d431853d3d31c0fa0b8ec29683
Soren Martius <soeren.martius@gmail.com>
2019-12-27 09:11:13 -0300

    add organization projects

--- modules/organization/main.tf
+++ modules/organization/main.tf
@@ -7,14 +7,28 @@ locals {
 }
 
 resource "github_membership" "membership" {
-  for_each = { for i in local.members : i.id => i }
+  for_each = { for m in local.members : m.id => m }
 
   username = each.value.username
   role     = each.value.role
 }
 
 resource "github_organization_block" "blocked_user" {
-  for_each = toset(var.blocked_users)
+  for_each = var.blocked_users
 
   username = each.value
+}
+
+locals {
+  projects = [for p in var.projects : merge({
+    id   = lower(p.name)
+    body = null
+  }, p)]
+}
+
+resource "github_organization_project" "project" {
+  for_each = { for p in local.projects : p.id => p }
+
+  name = each.value.name
+  body = each.value.body
 }
--- modules/organization/variables.tf
+++ modules/organization/variables.tf
@@ -1,5 +1,5 @@
 variable "blocked_users" {
-  type        = list(string)
+  type        = set(string)
   description = "A list of usernames to be blocked from a GitHub organization."
   default     = []
 }
@@ -11,8 +11,8 @@ variable "members" {
   # Terraform version. See https://github.com/hashicorp/terraform/issues/19898 and https://github.com/hashicorp/terraform/issues/22449
   #
   # type = list(object({
-  #  username = string
-  #  role     = optional(string)
+  #   username = string
+  #   role     = optional(string)
   # }))
   description = "A list of users to be added from your organization. When applied, an invitation will be sent to the user to become part of the organization. When destroyed, either the invitation will be cancelled or the user will be removed. Role must be one of member or admin. Defaults to member."
   default     = []
@@ -26,6 +26,31 @@ variable "members" {
   #   {
   #     username   = "username2"
   #     role       = "admin"
+  #   }
+  # ]
+}
+
+variable "projects" {
+  type = list(any)
+
+  # We can't use a detailed type specification due to a terraform limitation. However, this might be changed in a future
+  # Terraform version. See https://github.com/hashicorp/terraform/issues/19898 and https://github.com/hashicorp/terraform/issues/22449
+  #
+  # type = list(object({
+  #   name = string
+  #   body = optional(string)
+  # }))
+  description = "Create and manage projects for the GitHub organization."
+  default     = []
+
+  # Example:
+  # projects = [
+  #   {
+  #     name   = "Test Project"
+  #     body   = "This is a test project created by Terraform"
+  #   },
+  #   {
+  #     name   = "Test Project without a body"
   #   }
   # ]
 }
