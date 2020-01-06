8337e862f162bc3694a9f013501456e13dade8ea
Soren Martius <soeren.martius@gmail.com>
2020-01-06 08:03:08 -0300

    remove unnecessary Vars declaration from test

--- test/github_repository_test.go
+++ test/github_repository_test.go
@@ -12,6 +12,10 @@ func init() {
 	githubOrganization = os.Getenv("GITHUB_ORGANIZATION")
 	githubToken = os.Getenv("GITHUB_TOKEN")
 
+	if githubOrganization == "" {
+		panic("Please set a github organization using the GITHUB_ORGANIZATION environment variable.")
+	}
+
 	if githubToken == "" {
 		panic("Please set a github token using the GITHUB_TOKEN environment variable.")
 	}
