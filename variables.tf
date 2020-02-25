1b2e8908604963a9c8bbba10ba3077abedeeb576
Soren Martius <soeren.martius@gmail.com>
2020-02-25 10:31:27 +0100

    use sets instead of lists of unique parameters

--- variables.tf
+++ variables.tf
@@ -1,3 +1,26 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# GITHUB_ORGANIZATION
+# GITHUB_TOKEN
+
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED VARIABLES
+# These variables must be set when using this module.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "name" {
+  type        = string
+  description = "The name of the repository."
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL VARIABLES
+# These variables have defaults, but may be overridden.
+# ---------------------------------------------------------------------------------------------------------------------
+
 variable "defaults" {
   type        = any
   description = "Overwrite defaults for various repository settings"
@@ -22,184 +45,180 @@ variable "defaults" {
   # }
 }
 
-variable "name" {
-  type        = string
-  description = "The name of the repository."
-}
-
 variable "description" {
-  type        = string
   description = "A description of the repository."
+  type        = string
   default     = ""
 }
 
 variable "homepage_url" {
+  description = "The website of the repository."
   type        = string
-  description = "homepage_url"
   default     = null
 }
 
 variable "private" {
-  type        = bool
   description = "Set to false to create a public repository. (Default: true)"
+  type        = bool
   default     = null
 }
 
 variable "has_issues" {
-  type        = bool
   description = "Set to true to enable the GitHub Issues features on the repository. (Default: false)"
+  type        = bool
   default     = null
 }
 
 variable "has_projects" {
-  type        = bool
   description = "Set to true to enable the GitHub Projects features on the repository. Per the github documentation when in an organization that has disabled repository projects it will default to false and will otherwise default to true. If you specify true when it has been disabled it will return an error.  (Default: false)"
+  type        = bool
   default     = null
 }
 
 variable "has_wiki" {
-  type        = bool
   description = "Set to true to enable the GitHub Wiki features on the repository. (Default: false)"
+  type        = bool
   default     = null
 }
 
 variable "allow_merge_commit" {
-  type        = bool
   description = "Set to false to disable merge commits on the repository. (Default: true)"
+  type        = bool
   default     = null
 }
 
 variable "allow_squash_merge" {
-  type        = bool
   description = "Set to true to enable squash merges on the repository. (Default: false)"
+  type        = bool
   default     = null
 }
 
 variable "allow_rebase_merge" {
-  type        = bool
   description = "Set to true to enable rebase merges on the repository. (Default: false)"
+  type        = bool
   default     = null
 }
 
 variable "has_downloads" {
-  type        = bool
   description = "Set to true to enable the (deprecated) downloads features on the repository. (Default: false)"
+  type        = bool
   default     = null
 }
 
 variable "auto_init" {
-  type        = bool
   description = "Wether or not to produce an initial commit in the repository. (Default: true)"
+  type        = bool
   default     = null
 }
 
 variable "gitignore_template" {
-  type        = string
   description = "Use the name of the template without the extension. For example, Haskell. Available templates: https://github.com/github/gitignore"
+  type        = string
   default     = null
 }
 
 variable "license_template" {
-  type        = string
   description = "Use the name of the template without the extension. For example, 'mit' or 'mpl-2.0'. Available licences: https://github.com/github/choosealicense.com/tree/gh-pages/_licenses"
+  type        = string
   default     = null
 }
 
 variable "default_branch" {
-  type        = string
   description = "The name of the default branch of the repository. NOTE: This can only be set after a repository has already been created, and after a correct reference has been created for the target branch inside the repository. This means a user will have to omit this parameter from the initial repository creation and create the target branch inside of the repository prior to setting this attribute."
+  type        = string
   default     = null
 }
 
 variable "archived" {
-  type        = bool
   description = "Specifies if the repository should be archived. (Default: false)"
+  type        = bool
   default     = false
 }
 
 variable "topics" {
-  type        = list(string)
   description = "The list of topics of the repository.  (Default: [])"
+  type        = set(string)
   default     = null
 }
 
 variable "extra_topics" {
-  type        = list(string)
   description = "The list of additional topics of the repository. (Default: [])"
+  type        = set(string)
   default     = []
 }
 
 variable "template" {
+  description = "Template repository to use. (Default: {})"
   type = object({
     owner      = string
     repository = string
   })
-  description = "Template repository to use. (Default: {})"
-  default     = null
+  default = null
 }
 
 variable "admin_collaborators" {
-  type        = list(string)
   description = "A list of users to add as collaborators granting them admin (full) permission."
+  type        = set(string)
   default     = []
 }
 
 variable "push_collaborators" {
-  type        = list(string)
   description = "A list of users to add as collaborators granting them push (read-write) permission."
+  type        = set(string)
   default     = []
 }
 
 variable "pull_collaborators" {
-  type        = list(string)
   description = "A list of users to add as collaborators granting them pull (read-only) permission."
+  type        = set(string)
   default     = []
 }
 
 variable "triage_collaborators" {
-  type        = list(string)
   description = "A list of users to add as collaborators granting them triage permission."
+  type        = set(string)
   default     = []
 }
 
 variable "maintain_collaborators" {
-  type        = list(string)
   description = "A list of users to add as collaborators granting them maintain permission."
+  type        = set(string)
   default     = []
 }
 
 variable "admin_team_ids" {
-  type        = list(string)
   description = "A list of teams (by id) to grant admin (full) permission to."
+  type        = set(string)
   default     = []
 }
 
 variable "push_team_ids" {
-  type        = list(string)
   description = "A list of teams (by id) to grant push (read-write) permission to."
+  type        = set(string)
   default     = []
 }
 
 variable "pull_team_ids" {
-  type        = list(string)
   description = "A list of teams (by id) to grant pull (read-only) permission to."
+  type        = set(string)
   default     = []
 }
 
 variable "triage_team_ids" {
-  type        = list(string)
   description = "A list of teams (by id) to grant triage permission to."
+  type        = set(string)
   default     = []
 }
 
 variable "maintain_team_ids" {
-  type        = list(string)
   description = "A list of teams (by id) to grant maintain permission to."
+  type        = set(string)
   default     = []
 }
 
 variable "branch_protections" {
-  type = any
+  description = "Configuring protected branches. For details please check: https://www.terraform.io/docs/providers/github/r/branch_protection.html"
+  type        = any
 
   # We can't use a detailed type specification due to a terraform limitation. However, this might be changed in a future
   # Terraform version. See https://github.com/hashicorp/terraform/issues/19898 and https://github.com/hashicorp/terraform/issues/22449
@@ -225,8 +244,7 @@ variable "branch_protections" {
   #   })
   # }))
 
-  description = "Configuring protected branches. For details please check: https://www.terraform.io/docs/providers/github/r/branch_protection.html"
-  default     = []
+  default = []
 
   # Example:
   # branch_protections = [
@@ -257,19 +275,18 @@ variable "branch_protections" {
 }
 
 variable "issue_labels_merge_with_github_labels" {
-  type        = bool
   description = "Specify if you want to merge and control githubs default set of issue labels."
+  type        = bool
   default     = null
 }
 
 variable "issue_labels" {
+  description = "Configure a GitHub issue label resource."
   type = list(object({
     name        = string
     description = string
     color       = string
   }))
-  description = "Configure a GitHub issue label resource."
-  default     = []
 
   # Example:
   # issue_labels = [
@@ -284,12 +301,13 @@ variable "issue_labels" {
   #     color       = "1dc34f"
   #   }
   # ]
+
+  default = []
 }
 
 variable "deploy_keys" {
-  type        = any
   description = "Configure a deploy key ( SSH key ) that grants access to a single GitHub repository. This key is attached directly to the repository instead of to a personal user account."
-  default     = []
+  type        = any
 
   # Example:
   # deploy_keys = [
@@ -304,12 +322,13 @@ variable "deploy_keys" {
   #     read_only = false
   #   }
   # ]
+
+  default = []
 }
 
 variable "deploy_keys_computed" {
-  type        = any
   description = "Configure a deploy key ( SSH key ) that grants access to a single GitHub repository. This key is attached directly to the repository instead of to a personal user account."
-  default     = []
+  type        = any
 
   # Example:
   # deploy_keys_computed = [
@@ -319,15 +338,16 @@ variable "deploy_keys_computed" {
   #     read_only = true
   #   }
   # ]
+
+  default = []
 }
 
 variable "projects" {
+  description = "Create and manage projects for GitHub repository."
   type = list(object({
     name = string
     body = string
   }))
-  description = "Create and manage projects for GitHub repository."
-  default     = []
 
   # Example:
   # projects = [
@@ -336,4 +356,6 @@ variable "projects" {
   #     body = "This is a fancy test project for testing"
   #   }
   # ]
+
+  default = []
 }
