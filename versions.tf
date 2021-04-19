3c508a4d53aad749d478a2f3b1018324caa87544
Marius Tolzmann <m@rius.berlin>
2021-04-19 04:23:22 +0200

    feat: Remove support for unsecure providers versions

--- versions.tf
+++ versions.tf
@@ -5,7 +5,9 @@
 terraform {
   required_version = ">= 0.12.20, < 0.15"
 
+  # 4.7.0 to 4.9.1 has a security regression: new repositories created via a template have a public visibility
+  # fixed in 4.9.2
   required_providers {
-    github = ">= 4.3, < 5.0"
+    github = ">= 4.5, < 5.0, != 4.7.0, != 4.8.0, != 4.9.0, != 4.9.1"
   }
 }
