225e582516ebf9747f7d71df10d77d40bc60d469
Soren Martius <soeren.martius@gmail.com>
2021-05-18 23:31:05 +0200

    feat: add support for terraform v0.15

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 terraform {
-  required_version = ">= 0.12.20, < 0.15"
+  required_version = ">= 0.12.20, < 0.16"
 
   # 4.7.0 to 4.9.1 has a security regression: new repositories created via a template have a public visibility
   # fixed in 4.9.2
