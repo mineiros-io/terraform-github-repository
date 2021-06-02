4632d08aed3e24ac2e93404190d034ebc0608c4d
Marius Tolzmann <m@rius.berlin>
2021-06-02 16:24:05 +0200

    fix: trick?

--- test/unit-complete/main.tf
+++ test/unit-complete/main.tf
@@ -94,7 +94,7 @@ module "repository" {
       required_pull_request_reviews = {
         dismiss_stale_reviews = true
         dismissal_users       = [var.team_user]
-        # dismissal_teams                 = [github_team.team.slug]
+        dismissal_teams                 = [replace(lower(github_team.team.name), "/[^a-z0-9]/", "-")]
         require_code_owner_reviews      = true
         required_approving_review_count = 1
       }
@@ -102,7 +102,7 @@ module "repository" {
       restrictions = {
         users = [var.team_user]
         teams = [
-          # github_team.team.slug
+          replace(lower(github_team.team.name), "/[^a-z0-9]/", "-")
         ]
       }
     },
