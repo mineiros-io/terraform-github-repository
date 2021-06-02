2c03fbabb49258b6b8df64b7b2f4d8ba19d23860
Marius Tolzmann <m@rius.berlin>
2021-06-02 15:53:38 +0200

    fmt

--- test/unit-complete/main.tf
+++ test/unit-complete/main.tf
@@ -92,8 +92,8 @@ module "repository" {
       }
 
       required_pull_request_reviews = {
-        dismiss_stale_reviews           = true
-        dismissal_users                 = [var.team_user]
+        dismiss_stale_reviews = true
+        dismissal_users       = [var.team_user]
         # dismissal_teams                 = [github_team.team.slug]
         require_code_owner_reviews      = true
         required_approving_review_count = 1
